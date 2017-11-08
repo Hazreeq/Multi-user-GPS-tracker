f588d7ab06f4d8d24477ed7162c2e27a9f634b6b
Hazreeq <33491173+Hazreeq@users.noreply.github.com>
2017-11-09 00:12:23 +0800

    Add files via upload

---
+++ js/config.dist.js
@@ -0,0 +1,7 @@
+var config = {
+  firebase: 'gpstest-177201',
+  mapbox: {
+      accessToken: 'pk.eyJ1IjoiaGF6cmVlcSIsImEiOiJjajkydGUzbW0wc203MzBxYnM4cGFia2tuIn0.bIq7eMZU72PzPyoLkd1JYA',
+      mapId: 'mapbox.streets',
+  }
+}
