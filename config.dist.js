b00ecebb796038b4863748c4f639ca033e78f010
Hazreeq <33491173+Hazreeq@users.noreply.github.com>
2017-11-14 01:40:52 +0800

    Add files via upload

---
+++ config.dist.js
@@ -0,0 +1,7 @@
+var config = {
+  firebase: 'gps-realtime-tracking',
+  mapbox: {
+      accessToken: 'pk.eyJ1IjoiZGFuaWFpc21haWwiLCJhIjoiY2o2eXQyYTlwMGJ3azJxcWs3bnczZWJpYyJ9.5juGWuPP9Too0hJ4GQ_iVg',
+      mapId: 'mapbox.light',
+  }
+}
