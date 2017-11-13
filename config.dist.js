7cb4c76db5ddceb445053994a44f06c847333edb
Hazreeq <33491173+Hazreeq@users.noreply.github.com>
2017-11-14 02:57:00 +0800

    Update config.dist.js

--- config.dist.js
+++ config.dist.js
@@ -2,6 +2,6 @@ var config = {
   firebase: 'multiuser-gps-tracker',
   mapbox: {
       accessToken: 'pk.eyJ1IjoiZGFuaWFpc21haWwiLCJhIjoiY2o2eXQyYTlwMGJ3azJxcWs3bnczZWJpYyJ9.5juGWuPP9Too0hJ4GQ_iVg',
-      mapId: 'mapbox.light',
+      mapId: 'mapbox.streets',
   }
 }
