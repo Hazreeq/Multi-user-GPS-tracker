8680247b04e237446fecac0dbc5b79de76a3f9ac
Hazreeq <33491173+Hazreeq@users.noreply.github.com>
2017-11-14 01:20:31 +0800

    Delete config.dist.js

--- js/config.dist.js
+++
@@ -1,7 +0,0 @@
-var config = {
-  firebase: 'multiuser-gps-tracker',
-  mapbox: {
-      accessToken: 'pk.eyJ1IjoiaGF6cmVlcSIsImEiOiJjajkydGUzbW0wc203MzBxYnM4cGFia2tuIn0.bIq7eMZU72PzPyoLkd1JYA',
-      mapId: 'mapbox.streets',
-  }
-}
